9f10ae9689952f15dbfb08506a03a6f486f64e2f
Vigneshwaran <vigneshwaran_babu@maseke.com>
2025-07-22 09:06:16 -0400

    Added containers

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-# Make migrations and migrate the database.
-echo "Making migrations and migrating the database. "
+echo "Running Django migrations..."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+# Check if DB file exists (for debugging)
+ls -l db.sqlite3
+
+# Run final CMD (gunicorn)
 exec "$@"
